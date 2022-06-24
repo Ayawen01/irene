d7fc4baa3711e5ec52c6004274a933ba6d87a721
ayawen <52957307+Ayawen01@users.noreply.github.com>
2022-06-24 14:15:43 +0800

    14:15

--- irene.js
+++ irene.js
@@ -1,1 +1,23 @@
-console.log('hello irene.');
+"use script";
+
+function IreneVisit(x) {
+    this.value = x;
+    this.type = Object.prototype.toString.call(x).slice(8, -1);
+}
+
+IreneVisit.prototype.execute = function() {
+    switch (this.type) {
+        case 'String': return this.visitString();
+    }
+};
+
+IreneVisit.prototype.visitString = function() {
+    
+};
+
+function Irene(x) {
+    const visit = new IreneVisit(x);
+    return visit.execute();
+}
+
+window.$irene = (x) => new Irene(x);
